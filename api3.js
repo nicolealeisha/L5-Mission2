179cb3d2b3d5ca48b0a59f142fdd2e80e83ba15d
Nicole Smith <nicolealeisha@gmail.com>
2025-03-11 17:17:22 +1300

    nicoles API3 additions

---
+++ api3.js
@@ -0,0 +1,50 @@
+
+// 1. Require dependencies
+const express = require("express");
+const cors = require('cors');
+
+// 2. Create the new express app instance for our API
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// 3. Define routes
+app.get("/greet", (req, res) => {
+  const name = req.query.name || "World";
+  res.json({ message: `Hello, ${name}!` });
+});
+
+
+app.post('/api3', (req,res) =>{
+  const carValue = parseFloat(req.body.car_value);
+  const riskRating = parseFloat(req.body.risk_rating);
+  const yearlyPremium = carValue * riskRating / 100;
+  const monthlyPremium = yearlyPremium / 12;
+  
+
+  //capture all for error handling
+  if (carValue <= 0 || riskRating <= 0 || riskRating > 5 || isNaN(carValue) || isNaN(riskRating)){
+    return res
+    .status(400)
+    .json({error: 'There is an error'})
+  }
+
+  else {
+    console.log(req.body);
+    res
+    .status(200)
+    .json({
+      monthly_premium: parseFloat(monthlyPremium.toFixed(1)),
+      yearly_premium: parseFloat(yearlyPremium.toFixed(0)),
+    });
+}
+
+});
+
+
+
+
+
+
+// 4. Export the app for testing later
+module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // 1. Import our app from the app.js file
 const app = require("./app");
+const api3 = require("./api3");
 
 // 2. Start the server on port 3000
 app.listen(3000, () => {
